a1fd56948e58d467015f671f9bb41d67eb87f9e9
Marcio R <Murcielago203@gmail.com>
2022-02-03 15:31:12 -0500

    created updatetime function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,22 @@
 
 // Show current date 
 function main() {
-    $('#currentDay').text(moment().format('dddd MMMM do YYYY'));
-    
+    $('#currentDay').text(moment().format('dddd MMMM do YYYY'));  
+    updateTime();
+}
+
+// select hour compare to time, and update textarea color 
+function updateTime() {
+    $('.hour').each(function() {
+        // get hour 
+        var hour = moment($(this).text().trim(), 'LT');
+        console.log(hour);
+
+        // compare to hour to current time
+        var compareHour = hour.isBefore(moment());
+        console.log(compareHour);
+        // insert a new class into text area 
+    })
 }
 
 $(document).ready(main)
