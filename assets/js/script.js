122a7b3e90d444c3c5a2cc4073c5fe9b63b0a3ca
Marcio R <Murcielago203@gmail.com>
2022-02-03 15:45:02 -0500

    fixed updateTime funchtion to change colors during past , present , future timeslots to reflect the current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,23 @@
 function main() {
     $('#currentDay').text(moment().format('dddd MMMM do YYYY'));  
     updateTime();
-}
+};
 
 // select hour compare to time, and update textarea color 
 function updateTime() {
     $('.hour').each(function() {
         // get hour 
         var hour = moment($(this).text().trim(), 'LT');
-        console.log(hour);
-
         // compare to hour to current time
-        var compareHour = hour.isBefore(moment());
+        var compareHour = Math.ceil(moment.duration(hour.diff(moment())).asHours());
         console.log(compareHour);
         // insert a new class into text area 
-    })
-}
+        if(compareHour < 0) {
+            $(this).next().addClass('past')
+        } else if (compareHour == 0) {
+            $(this).next().addClass('present');
+        } else ($(this).next().addClass('future'))
+    });
+};
 
 $(document).ready(main)
