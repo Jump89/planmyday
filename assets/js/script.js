38583b54af0a31be94478eee47e12330d73a0fa0
Marcio R <Murcielago203@gmail.com>
2022-02-03 16:11:25 -0500

    create savebtn function to save to local storage with 3 var

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 
-
 // Show current date 
 function main() {
     $('#currentDay').text(moment().format('dddd MMMM do YYYY'));  
     updateTime();
+    $(document).on('click','.saveBtn', saveBtn)
 };
 
 // select hour compare to time, and update textarea color 
@@ -13,7 +13,6 @@ function updateTime() {
         var hour = moment($(this).text().trim(), 'LT');
         // compare to hour to current time
         var compareHour = Math.ceil(moment.duration(hour.diff(moment())).asHours());
-        console.log(compareHour);
         // insert a new class into text area 
         if(compareHour < 0) {
             $(this).next().addClass('past')
@@ -22,5 +21,12 @@ function updateTime() {
         } else ($(this).next().addClass('future'))
     });
 };
+
+// added save button function 
+function saveBtn(event) {
+    var textArea = $(event.target).siblings('.description').val(); /// grabs sibling and class input data
+    var hourParent = $(event.target).parent('.time-block').attr('id'); /// grabs parent and class id attribute for hours 
+    localStorage.setItem(hourParent, textArea);
+}
 
 $(document).ready(main)
